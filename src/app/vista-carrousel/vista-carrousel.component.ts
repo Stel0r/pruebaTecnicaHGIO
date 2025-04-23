56e17d8671e1ba782d0dc519bf83b641bf33ef5a
Diego Gamez <65517739+Stel0r@users.noreply.github.com>
2025-04-22 19:05:54 -0500

    implementada el nombre y desctipcion de la raza implementada la funcionalidad del carusel de imagenes

--- src/app/vista-carrousel/vista-carrousel.component.ts
+++ src/app/vista-carrousel/vista-carrousel.component.ts
@@ -11,8 +11,10 @@ import { RazasService } from '../services/razas.service';
 })
 
 export class VistaCarrouselComponent {
-  texto:string = "Bienvenido :3";
+  nombreRaza:string = "";
+  descripcionRaza:string = "";
   breedArray:Array<breedInfo> = [];
+  imagenInicial:number = 0;
   constructor(private http: HttpClient,private razasService: RazasService) {
     // this.http.get('https://jsonplaceholder.typicode.com/posts').subscribe(data => {
     //   console.log(data);
@@ -23,8 +25,24 @@ export class VistaCarrouselComponent {
     this.cargarRazas()
   }
 
+  actualizarDescripcion() {
+    this.nombreRaza = this.breedArray[this.imagenInicial+1].name
+    this.descripcionRaza = this.breedArray[this.imagenInicial+1].description
+  }
+
+  moverIzquierda() {
+    this.imagenInicial = (this.imagenInicial == -1) ? this.breedArray.length-2:this.imagenInicial-1
+    this.actualizarDescripcion()
+  }
+
+  moverDerecha() {
+    this.imagenInicial = this.imagenInicial = (this.imagenInicial == this.breedArray.length-2) ? -1:this.imagenInicial+1
+    this.actualizarDescripcion()
+  }
+
   async cargarRazas() {
     this.breedArray = await this.razasService.getRazas()
+    this.actualizarDescripcion()
   }
 }
 
